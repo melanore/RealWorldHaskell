1c0d1d3699d3a51c83b916d181209428a665ad03
melanore <max@socialcee.com>
2017-08-17 01:05:22 +0200

    ch03: cleanup

--- ch03/Guard.hs
+++ ch03/Guard.hs
@@ -9,29 +9,3 @@ skip n xs
     | xs == []  = []
 skip n (_:xs)       = skip (n - 1) xs
 
--- exercises
---1-2.
-count :: Num t => [a] -> t
-count []       = 0
-count (_:xs)   = 1 + (count xs)
-
---3.
-mean :: Fractional t => [t] -> t
-mean []        = 0
-mean xs        = mySum / myLen
-    where 
-        mySum = sum xs
-        myLen = fromIntegral $ length xs
-        
---4.
-palindromize []     = []
-palindromize list   = list ++ (reverse list)
-        where 
-            reverse []      = []
-            reverse (x:xs)  = (reverse xs) ++ [x]
-
---5.
-isPalindrome :: Eq a => [a] -> Bool
-isPalindrome []  = True
-isPalindrome [_] = True
-isPalindrome xs  = (head xs) == (last xs) && (isPalindrome (tail xs))
